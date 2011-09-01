e64105e8a82561b0b32756872384724353ac1852
ap4y <lod@pisem.net>
2011-09-01 12:04:18 +0400

    Project creation
    iOS part created

---
+++ Cococa/KageClient/KageClient/KageClientAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  KageClientAppDelegate.h
+//  KageClient
+//
+//  Created by Arthur Evstifeev on 01.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KageClientAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
