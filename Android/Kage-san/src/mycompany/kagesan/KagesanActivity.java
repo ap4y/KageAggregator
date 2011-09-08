b876205e22c97eb904e6396aa31dc25a66d82adc
ap4y <lod@pisem.net>
2011-09-08 19:41:17 +0400

    Android template added

---
+++ Android/Kage-san/src/mycompany/kagesan/KagesanActivity.java
@@ -0,0 +1,13 @@
+package mycompany.kagesan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class KagesanActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
